1dfbb41a7fa9311b9f43774caffe685715536a7f
Den1eD1 <gabo.dimitrov@gmail.com>
2015-11-19 21:46:39 +0200

    changed front controller

--- Framework/FrontController.php
+++ Framework/FrontController.php
@@ -5,6 +5,9 @@ namespace Framework;
 class FrontController
 {
     private static $_instance = null;
+    private $ns = null;
+    private $controller = null;
+    private $method = null;
 
     private function __construct(){
 
@@ -14,6 +17,24 @@ class FrontController
         $a = new \Framework\Routers\DefaultRouter();
         $_uri = $a->getURI();
         $routes = \Framework\App::getInstance()->getConfig()->routes;
+        if(is_array($routes) && count($routes) > 0) {
+            foreach( $routes as $k => $v) {
+                if(stripos($_uri, $k) === 0 && $v['namespace']) {
+                    $this->ns = $v['namespace'];
+                    break;
+                }
+            }
+        } else {
+            throw new \Exception('Default route missing', 500);
+        }
+
+        if ($this->ns == null && $routes['*']['namespace']) {
+            $this->ns = $routes['*']['namespace'];
+        } else if ($this->ns == null && !$routes['*']['namespace']) {
+            throw new \Exception('Default route missing', 500);
+        }
+
+        echo $this->ns;
     }
 
     public function getDefaultController() {
